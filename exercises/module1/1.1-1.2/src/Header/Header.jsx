9643d2a9a717ecd1cf9d2656fa53b313f81059b5
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-09-23 11:31:36 +0200

    1.1-1.2 : import image

--- exercises/module1/1.1-1.2/src/Header/Header.jsx
+++ exercises/module1/1.1-1.2/src/Header/Header.jsx
@@ -1,6 +1,9 @@
+import sc from "Header/star_citizen_Quantum_Drive.jpeg";
+
 const Header = (props) => {
   return (
       <div>
+        <img src={sc} alt={"Star_Citizen_Quantum_Drive"}/>
         <h1>{props.course}</h1>
       </div>
   );
