42bcc5233b3fe6e105a5509c7721b77ac3881c94
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-09-23 11:18:00 +0200

    1.1-1.2 : refactor - absolute imports

--- exercises/module1/1.1-1.2/src/App/App.js
+++ exercises/module1/1.1-1.2/src/App/App.js
@@ -1,6 +1,6 @@
-import Header from "../Header/Header";
-import Content from "../Content/Content";
-import Total from "../Total/Total";
+import Header from "Header/Header";
+import Content from "Content/Content";
+import Total from "Total/Total";
 
 const App = () => {
   const course = 'Half Stack application development'
